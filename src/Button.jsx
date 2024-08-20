ff0701503740a884cac9e409d4b4eb1905817d22
Thomas McElmeel <helloitisthomasm@gmail.com>
2024-08-20 14:02:48 -0400

    Css Styling

---
+++ src/Button.jsx
@@ -0,0 +1,13 @@
+function Button() {
+  const styles = {
+    backgroundColor: "blue",
+    color: "white",
+    padding: "10px",
+    borderRadius: "5px",
+    border: "none",
+    cursor: "pointer",
+  };
+  return <button style={styles}>Click me</button>;
+}
+
+export default Button;
