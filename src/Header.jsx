f6f895c49785f23f0d7c397648fa8057925fd05d
Thomas McElmeel <helloitisthomasm@gmail.com>
2024-08-20 13:37:45 -0400

    init

---
+++ src/Header.jsx
@@ -0,0 +1,23 @@
+function Header() {
+  return (
+    <header>
+      <h1>This is my Header</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+      <hr />
+    </header>
+  );
+}
+
+export default Header;
