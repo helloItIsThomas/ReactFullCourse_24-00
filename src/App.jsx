8f59f39d20b37f47ff689b0483b968d12ad72964
Thomas McElmeel <helloitisthomasm@gmail.com>
2024-08-20 16:24:10 -0400

    done with props

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import Footer from "./Footer.jsx";
 import Food from "./Food.jsx";
 import Card from "./Card.jsx";
 import Button from "./Button.jsx";
+import Student from "./Students.jsx";
 
 function App() {
   return (
     <>
-      <Button />
-      <Card />
+      <Student name="Garbo" age={4} isStudent={true} />
+      <Student name="Warbo" age={6} isStudent={false} />
+      {/* <Button /> */}
+      {/* <Card /> */}
     </>
   );
 }
